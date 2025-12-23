10e3d02ca90a1c7364184bf1036a8f1c41aed638
gerep <181501+gerep@users.noreply.github.com>
2025-12-23 20:36:41 -0300

    burst increases blob speed

--- scenes/blob/blob.gd
+++ scenes/blob/blob.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
+const SPEED: float = 200.0
+
 var current_color: int
+var speed_multiplier: float = 1.0
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
@@ -10,6 +13,9 @@ func _ready() -> void:
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
 	body_entered.connect(_collision)
 
+	Signals.burst_started.connect(_burst_started)
+	Signals.burst_ended.connect(_burst_ended)
+
 	position.x = randf_range(36.0, 700.0)
 
 	current_color = randi_range(0, Colors.LIST.size() - 1)
@@ -17,7 +23,7 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	position.y += 200 * delta
+	position.y += SPEED * delta * speed_multiplier
 
 
 func _collision(body: Node2D) -> void:
@@ -28,3 +34,11 @@ func _collision(body: Node2D) -> void:
 		pass
 
 	call_deferred(&"queue_free")
+
+
+func _burst_started() -> void:
+	speed_multiplier = 1.5
+
+
+func _burst_ended() -> void:
+	speed_multiplier = 1.0
